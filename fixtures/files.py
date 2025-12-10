d914ae7417f4657ba1a5a6b7114b0ac0c152e417
GETrofimov <trofimov.g98@gmail.com>
2025-12-10 10:36:26 +0500

    Настройки автотестов с Pydantic. Практика работы с настройками

--- fixtures/files.py
+++ fixtures/files.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from clients.files.files_client import FileClient, get_files_client
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
 from fixtures.users import UserFixture
+from config import settings
 
 
 class FileFixture(BaseModel):
@@ -17,6 +18,6 @@ def files_client(function_user: UserFixture) -> FileClient:
 
 @pytest.fixture
 def function_file(files_client: FileClient) -> FileFixture:
-    request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+    request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
     response = files_client.create_file(request)
     return FileFixture(request=request, response=response)
